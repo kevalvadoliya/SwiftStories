6a58ee3e5baf98832e4ae9a9eaf6b2c7f7b95399
Keval Vadoliya <kevivadoliya@gmail.com>
2025-05-22 17:04:08 +0530

    setup project structure

---
+++ SwiftStories/StoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryListView.swift
+//  SwiftStories
+//
+//  Created by Keval Vadoliya on 22/05/25.
+//
+
+import SwiftUI
+
+struct StoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryListView()
+}
---
+++ SwiftStories/StoryListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  StoryListViewModel.swift
+//  SwiftStories
+//
+//  Created by Keval Vadoliya on 22/05/25.
+//
+
+import SwiftUI
+
+class StoryListViewModel: ObservableObject {
+    
+}
